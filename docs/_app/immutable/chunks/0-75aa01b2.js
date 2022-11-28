1219fafa57fd21d5b032d1f0459c40637a8dcbae
HejJagHeterElias <elisoc@edu.umea.se>
2022-11-28 10:21:27 +0100

    first commit

---
+++ docs/_app/immutable/chunks/0-75aa01b2.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-68b25b1b.js";import{default as t}from"../components/pages/_layout.svelte-aebab23c.js";export{t as component,r as shared};
---
+++ docs/_app/immutable/chunks/4-269003c4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/telltale/_page.svelte-7ed4970b.js";export{t as component};
